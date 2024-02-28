a7bf35820e92587211d63b34239ea051f9b3e2d7
Vatsal Agarwal <103558712+vatsalagarwall@users.noreply.github.com>
2024-02-28 18:42:34 +0530

    Update index.js AGAIN LAST TIME

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: ["https://portfolio-vatsal.vercel.app/"],
+        origin: ["https://portfolio-vatsal.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
