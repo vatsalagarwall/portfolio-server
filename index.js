af1279684a9fde96988bc24e2972d334c8822a84
Vatsal Agarwal <103558712+vatsalagarwall@users.noreply.github.com>
2024-02-28 18:38:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: ["https://portfolio-client-8r3k.onrender.com"],
+        origin: ["https://portfolio-client-wine.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
